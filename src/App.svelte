ed2ea940fda537a093fda6864e998215b4ccc4f9
lookrecius <bragin.boris@ya.ru>
2020-03-28 02:06:40 +0300

    Added import Calculator

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
-<Calculator />
+<div>
+	<Calculator bind:result/>
+	<h1> 57C;LB0B, {result}!</h1>
+</div>
+
 <script>
 	import Calculator from './Calculator.svelte'
-	let name = 'Калькулятор';
+	let result;
 	
 </script>
 
-<h1>@Привет, {name}!</h1>
+
 <style>
-	
+		div {border: 1px solid green; padding:10px; color: red}
 </style>
