ef27f8eb2277facfdf5e816d76ed7a629544505f
lookrecius <bragin.boris@ya.ru>
2020-03-28 02:09:31 +0300

    Added src/Operations.svelte -> Array of operation

---
+++ src/Operations.svelte
@@ -0,0 +1,10 @@
+<select bind:value={value}>
+	{#each operationArr as value }
+		<option {value}>{value}</option>
+		{/each}
+	</select>
+
+<script>
+		const operationArr = ['+', '-', '*', '/'];
+		export let value = operationArr[0]
+</script>
