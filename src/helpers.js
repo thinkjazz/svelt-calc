186d274cde7205fb606b69420016b6fc1d6c7695
lookrecius <bragin.boris@ya.ru>
2020-03-28 02:08:02 +0300

    Add calculate() function

--- src/helpers.js
+++ src/helpers.js
@@ -1,18 +1,32 @@
-
+//Моя релизация с приведением к флоту
 export function calc(a, b, operation) {
   if(operation === '+') {
-    return (parseFloat(a) + parseFloat(b));
-  }
-  if(operation === '-') {
-    return (parseFloat(a) - parseFloat(b));
-  }
-  if(operation === '*') {
-    return (parseFloat(a) * parseFloat(b));
-  }
-  if(operation === '/') {
-    return ((parseFloat(a) / parseFloat(b)).toFixed(1));
-  }
+     return (parseFloat(a) + parseFloat(b));
+   }
+   if(operation === '-') {
+     return (parseFloat(a) - parseFloat(b));
+   }
+   if(operation === '*') {
+     return (parseFloat(a) * parseFloat(b));
+   }
+   if(operation === '/') {
+     return ((parseFloat(a) / parseFloat(b)).toFixed(1));
+   }
 
+ }
+
+
+//Пример Павла Малышева
+//export function calc(a, b, operation) {
+//return operation ? (new Function('a', 'b', `return a ${operation} b`))(a, b) : 0;
+//}
+export function calculate(vals = [], operation = []) {
+	let values = [...vals];
+	let val = values.shift();
+	return values.reduce((result, val, i) => calc(result, val, operation[i]), val);
 }
 
-calc();
+
+
+
+
